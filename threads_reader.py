22d1c3f1f3bfdb355640724e9118f78f57bbd0c8
Mel Nguyen (she/her) <12276986+meln-ds@users.noreply.github.com>
2025-01-17 23:04:27 +0700

    add instructions to comment

--- threads_reader.py
+++ threads_reader.py
@@ -30,13 +30,23 @@ browser = Browser(
 controller = Controller()
 
 agent = Agent(
+	#task = f"""Follow these instructions:
+	#1. Navigate to 'https://www.threads.net/' 
+	#2. Find the search icon on the list of icons on the left side of the screen.
+	#3. Click on the search icon
+	#4. In the search box, type 'datbike'. Hit Enter to load the posts.
+	#5. Read the post content on the page. Once you're done, scroll down and read more content. Repeat 2 more times.
+	#6. Return a paragraph summary (in English) of all content you read, and the number of posts.
+    #""",
 	task = f"""Follow these instructions:
-	1. Navigate to 'https://www.threads.net/' 
-	2. Find the search icon on the list of icons on the left side of the screen.
-	3. Click on the search icon
-	4. In the search box, type 'datbike'. Hit Enter to load the posts.
-	5. Read the post content on the page. Once you're done, scroll down and read more content. Repeat 2 more times.
-	6. Return a paragraph summary (in English) of all content you read, and the number of posts.
+    1. Navigate to 'https://www.threads.net/'
+	2. Click on the first post by clicking on the space right next to the three dot button.
+	3. Verify that you have indeed navigated to the URL of that post 
+	4. Read through the comments on the post for context. Scroll down if necessary
+	5. Scroll all the way back up
+	6. Click on the reply button
+	7. Generate a max 2 sentences reply. Reply in the same language as the post language.
+	8. Click "Post"
     """,
 	llm=llm,
 	controller=controller,
